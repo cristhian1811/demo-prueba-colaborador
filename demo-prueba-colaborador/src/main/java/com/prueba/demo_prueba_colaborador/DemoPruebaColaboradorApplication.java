f08367475413c856aee6cf9cdcd772869e69204a
Cristhian1811 <mv.cristhian1811@hotmail.com>
2025-03-12 19:13:36 -0500

    inicio

---
+++ demo-prueba-colaborador/src/main/java/com/prueba/demo_prueba_colaborador/DemoPruebaColaboradorApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.demo_prueba_colaborador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPruebaColaboradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPruebaColaboradorApplication.class, args);
+	}
+
+}
---
+++ demo-prueba-colaborador/src/test/java/com/prueba/demo_prueba_colaborador/DemoPruebaColaboradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.demo_prueba_colaborador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPruebaColaboradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
